124b22b5e7e95beec0cf16570bc10b37ddf37d6c
Diego Zertuche <diegozertuche@Diegos-MacBook.local>
2021-05-09 20:54:40 -0500

    Adding DE parallel file

--- DE_parallel.py
+++ DE_parallel.py
@@ -16,7 +16,7 @@ def DE():
 
     if rank == 0:
         exp_data = pd.read_csv('exp_data.csv', index_col='Unnamed: 0')
-        metadata = pd.read_csv('metadata.csv', index_col='Unnamed: 0')
+        metadata = pd.read_csv('metadata.tsv', index_col='Unnamed: 0')
         clusters = pd.read_csv('clusters.csv', index_col='Unnamed: 0')
 
         # Cluster array
@@ -67,33 +67,37 @@ def DE():
                 # Determine the data associated with the COVID group and healthy group
                 covid_group = cluster_exp[cluster_metadata['Sample Characteristic[disease]'].values == 'COVID-19']
                 healthy_group = cluster_exp[cluster_metadata['Sample Characteristic[disease]'].values == 'normal']
-                    
+
                     # Create lists used to store the log2 fold changes and p-values associated with each gene
                 log_FC_list, p_val_list = [],[]
                     # Iterate over every gene
                 for gene in cluster_exp.columns:
-                        
+
                         # Compute the log2 fold change
-                    log_FC = np.log2(np.mean(covid_group[gene].values)/np.mean(healthy_group[gene].values))
+                        log_FC = np.log2(np.mean(covid_group[gene].values)/np.mean(healthy_group[gene].values))
                     log_FC_list.append(log_FC)
                         # Perform a Wilcoxon rank sum test and pull the p-value
                     p_val = ranksums(covid_group[gene].values, healthy_group[gene].values)
                     p_val_list.append(p_val)
-                        
+
                 cluster_df = pd.DataFrame({'gene': exp_data_columns,
                                               'logFC': log_FC_list,
-                                              'p_val': p_val_list})    
+                                              'p_val': p_val_list})
                 cluster_df.to_csv("./differential_expression/cluster" + str(clust) + ".csv")
                 count += 1
 
-    return rank
-    
+    return rank, comm, size
+
 # Read in data
 
 ini = MPI.Wtime()
-rank = DE()
+rank, comm, size = DE()
+if rank == 0:
+    for i in range(size-1):
+        comm.recv(source=i+1, tag=33)
+else:
+    comm.send(0, dest=0, tag=33)
 end = MPI.Wtime()
 
 if rank == 0:
     print(end - ini)
-
